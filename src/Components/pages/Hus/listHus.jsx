4f4e50ba752933ae200b943543c3b20fa87a2fe3
Hamid Hamrah <89603079+HamidHamrah@users.noreply.github.com>
2023-11-29 11:10:10 +0100

    Input validation for list house-->All DONE

--- src/Components/pages/Hus/listHus.jsx
+++ src/Components/pages/Hus/listHus.jsx
@@ -21,12 +21,12 @@ const ListHus = () => {
     const [harParkering, setHarParkering] = useState(false);
     const [eierKontoNummer, setEierKontoNummer] = useState(0);
     const [erMoblert, setErMoblert] = useState(true);
-    const {user} = useContext(AuthContext);
-   
+    const { user } = useContext(AuthContext);
+
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-        
+
         try {
             const formData = new FormData();
             formData.append('Pris', pris);
@@ -41,19 +41,19 @@ const ListHus = () => {
             bildeListe.forEach((file) => formData.append('bilder', file));
             formData.append('bilderUrl', bilderUrl);
             formData.append('ordreListe', JSON.stringify([]));
-            
-            for (let [key, value] of formData.entries()) { 
-                console.log(key, value); 
+
+            for (let [key, value] of formData.entries()) {
+                console.log(key, value);
             }
-            
-            
+
+
             const response = await axios.post(`http://localhost:11569/api/Hus/CreateHouseWithImages?email=${encodeURIComponent(user.email)}`, formData, {});
-            if (response.status===200 && response.data.houseId){
+            if (response.status === 200 && response.data.houseId) {
 
                 const houseId = response.data.houseId;
                 navigate(`/oversikt/${houseId}`)
 
-                
+
 
             }
             console.log(response.data);
@@ -61,23 +61,23 @@ const ListHus = () => {
             console.error('There was an error posting the data', error);
         }
 
-        
+
 
     };
 
     const handleImageUpload = (event) => {
         console.log("File Input Changed")
-        const files = Array.from(event.target.files); 
+        const files = Array.from(event.target.files);
         console.log("Selected files,", files)
-        
+
         const newImageUrls = files.map(file => URL.createObjectURL(file));
-    
-        
+
+
         if (newImageUrls.length > 0) {
             setBilderUrl(newImageUrls[0]);
         }
-    
-        
+
+
         setBildeListe(prevBildeListe => [...prevBildeListe, ...files]);
     };
 
@@ -92,55 +92,55 @@ const ListHus = () => {
                                 <label className="form-check-label text-white" htmlFor="pris">
                                     Price
                                 </label>
-                                <div className="mb-3">
-                                    <input type="number" className="form-control" value={pris} onChange={(e) => setPris(e.target.value)} />
-                                </div>
-                                <label className="form-check-label text-white" htmlFor="beskrivelse">
-                                    Description
-                                </label>
-                                <div className="mb-3">
-                                    <input type="text" className="form-control" value={beskrivelse} onChange={(e) => setBeskrivelse(e.target.value)} />
-                                </div>
-                                <label className="form-check-label text-white" htmlFor="harParkering">
-                                    Amount of rooms
-                                </label>
-                                <div className="mb-3">
-                                <label className="form-check-label text-white" htmlFor="romAntall">
-                                    Number of Rooms
-                                </label>
                                 <input
                                     type="number"
-                                    id="romAntall"
+                                    className="form-control"
+                                    value={pris}
+                                    onChange={(e) => setPris(e.target.value)}
+                                    min="1"
+                                    max="10000"
+                                    step="0.01"
+                                    required
+                                />
+                                <label className="form-check-label text-white" htmlFor="romAntall">Number of Rooms</label>
+                                <input
+                                    type="number"
                                     className="form-control"
                                     value={romAntall}
                                     onChange={(e) => {
-                                        const newValue = parseInt(e.target.value, 10);
-                                        if (newValue >= 0 && newValue <= 20) {
-                                            setRomAntall(newValue);
-                                        } else if (newValue < 0) {
-                                            setRomAntall(0);
-                                        } else if (newValue > 20) {
-                                            setRomAntall(20);
-                                        }
+                                        const newValue = Math.max(0, Math.min(10000, parseFloat(e.target.value)));
+                                        setRomAntall(newValue);
                                     }}
                                     min="0"
                                     max="20"
+                                    step="0.01"
+                                    required
                                 />
-                            </div>
-                                <label className="form-check-label text-white" htmlFor="romAntall">
+                                <label className="form-check-label text-white" htmlFor="Adress">
                                     Address
                                 </label>
-                                <div className="mb-3">
-                                    <input type="text" className="form-control" value={adresse} onChange={(e) => setAdresse(e.target.value)} />
-                                </div>
-                                <label className="form-check-label text-white" htmlFor="adresse">
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    value={adresse}
+                                    onChange={(e) => setAdresse(e.target.value.trim())}
+                                    required
+                                    minLength="10"
+                                    maxLength="100"
+                                />
+
+                                <label className="form-check-label text-white" htmlFor="by">
                                     City
                                 </label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    value={by}
+                                    onChange={(e) => setBy(e.target.value)}
+                                    required
+                                />
 
-                                <div className="mb-3">
-                                    <input type="text" className="form-control" value={by} onChange={(e) => setBy(e.target.value)}  />
-                                </div>
-                                <label className="form-check-label text-white" htmlFor="by">
+                                <label className="form-check-label text-white" htmlFor="moblert">
                                     Furnished ?
                                 </label>
                                 <div className="mb-3 form-check">
@@ -149,7 +149,7 @@ const ListHus = () => {
                                         className="form-check-input"
                                         id="erMoblert"
                                         checked={erMoblert}
-                                        onChange={(e) => setErMoblert(e.target.checked)} 
+                                        onChange={(e) => setErMoblert(e.target.checked)}
                                     />
                                 </div>
                                 <label className="form-check-label text-white" htmlFor="harParkering">
@@ -168,15 +168,48 @@ const ListHus = () => {
                                     Owner Account Number
                                 </label>
                                 <div className="mb-3">
-                                    <input type="text" className="form-control" value={eierKontoNummer} onChange={(e) => setEierKontoNummer(e.target.value)}  />
-                                </div>
-                                <label className="form-check-label text-white" htmlFor="harParkering">
-                                    Size in sqm
-                                </label>
-                                <div className="mb-3">
-                                    <input type="text" className="form-control" value={areal} onChange={(e) => setAreal(e.target.value)}  />
+                                    <input
+                                        type="number"
+                                        className="form-control"
+                                        value={eierKontoNummer}
+                                        onChange={(e) => setEierKontoNummer(e.target.value)}
+                                        pattern="^\d{11}$"
+                                        title="Enter an 11-digit number"
+                                        required
+                                    />
                                 </div>
 
+                                <label className="form-check-label text-white" htmlFor="harParkering">
+                                    Size in m<sup>2</sup>
+                                </label>
+                                <div className="mb-3">
+                                    <input
+                                        type="number"
+                                        className="form-control"
+                                        value={areal}
+                                        onChange={(e) => {
+                                            // Allow only numbers and decimal points
+                                            const validValue = e.target.value.replace(/[^0-9.]/g, '');
+                                            // Update the state only if the new value is a valid number or empty
+                                            if (validValue === '' || !isNaN(validValue)) {
+                                                setAreal(validValue);
+                                            }
+                                        }}
+                                        required
+                                    />
+                                </div>
+
+                                <label className="form-check-label text-white" htmlFor="beskrivelse">
+                                    Description
+                                </label>
+                                <div className="mb-3">
+                                    <textarea
+                                        className="form-control"
+                                        value={beskrivelse}
+                                        onChange={(e) => setBeskrivelse(e.target.value)}
+                                        rows="5"
+                                    />
+                                </div>
                                 <label className="form-check-label text-white" htmlFor="harParkering">
                                     Upload Images
                                 </label>
